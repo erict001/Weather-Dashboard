76acdd82181fa1acc61e87b4688ba592906cad8d
Eric Taylor <eric.taylor1012@gmail.com>
2022-04-12 12:02:43 -0700

    forecast added

--- script.js
+++ script.js
@@ -1,43 +1,68 @@
-var city = document.querySelector("#user-input")
-var searchBtn = document.querySelector("#search-input")
-var localCityEl = document.querySelector("#city")
-var temperatureEl = document.querySelector('#temperature')
-var windSpeedEl = document.querySelector('#wind-speed')
-var humidityEl = document.querySelector('#humidty')
-var uvEl = document.querySelector('#uv-index')
-var ApiKey = "fa7d52ba051ca9699cdf472c349d267c"
+var city = document.getElementById("cityInput");
+var searchBtn = document.querySelector("#searchBtn");
+var localCityEl = document.querySelector("#city");
+var temperatureEl = document.querySelector('#temperature');
+var windSpeedEl = document.querySelector('#wind-speed');
+var humidityEl = document.querySelector('#humidity');
+var uvEl = document.querySelector('#uv-index');
+var ApiKey = "fa7d52ba051ca9699cdf472c349d267c";
 
-// search for the city
-function citySearch() {
-  var weatherDashboard = 'https://api.openweathermap.org/geo/1.0/direct?q='; 
-    fetch(weatherDashboard + city + "&appid=" + ApiKey)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-
-    // localCityEl.textContent = data[0].name;
-    // temperatureEl.textContent = data[0].current.temp;
-    // uvEl.textContent = data[0].current.uvi;
-    // windSpeedEl.textContent = data[0].current.wind_speed;
-    });
-  };
-
-  searchBtn.addEventListener('click', citySearch());
+//forecast data
+var forecastEl = document.getElementById("forecast")
+var forecastDatesEl = document.getElementById("forecast-dates")
+var forecastTempEl = document.getElementById("forecast-temperature")
+var forecastWindEl = document.getElementById("forecast-wind-speed")
+var forecastHumidityEl = document.getElementById("forecast-humidity")
 
 
-  // //search for temperature
-  // function coordinates () {
-  //   var weatherDashboard = 'https://api.openweathermap.org/geo/1.0/direct?q='; 
-  //     fetch(weatherDashboard)
-  //     .then(function (response) {
-  //       return response.json();
-  //     })
-  //     .then(function (data) {
-  //     temperatureEl.textContent = localCity[0].current.temp
-      
-  //     });
-  // }
+  // search for city and other data
+  function apiParameters() {
+    let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city.value + "&appid=" + ApiKey; 
+      fetch(queryURL)
+      .then(function(getApi){
+        return getApi.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        localCityEl.textContent = data.name;
+        temperatureEl.textContent = data.main.temp + " deg C";
+        windSpeedEl.textContent = data.wind.speed + " MPH";
+        humidityEl.textContent = data.main.humidity + " %";
+
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+
+      return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=" + ApiKey)
+      }) .then(function (response) {
+          return response.json();
+      })  
+      .then (function (data1) {
+      console.log(data1)
+      uvEl.textContent = data1.current.uvi
+
+      var lat1 = data1.lat;
+      var lon1 = data1.lon;
+
+      return fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat1 + "&lon=" + lon1 + "&appid=" + ApiKey)
+      }) .then(function (forecast) {
+          return forecast.json();
+      })  
+      .then (function (forecastData) {
+      console.log(forecastData)
+      forecastEl.textContent = forecastData.city.name
+      forecastTempEl.textContent = forecastData.list[0].main.temp + " deg C"
+      forecastWindEl.textContent = forecastData.list[0].wind.speed + "MPH"
+      forecastHumidityEl.textContent = forecastData.list[0].main.humidity + " %"
+
+      // forecastEl.textContent = forecastData.city.name
+      // forecastTempEl.textContent = forecastData.list[1].main.temp + " deg C"
+      // forecastWindEl.textContent = forecastData.list[1].wind.speed + "MPH"
+      // forecastHumidityEl.textContent = forecastData.list[1].main.humidity + " %"
+      });
+    };
+  
+    searchBtn.addEventListener('click', apiParameters);
+
+
 
 
