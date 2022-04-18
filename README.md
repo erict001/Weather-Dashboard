d177652174e59eaf70df2e338803008b31b2e41b
Eric Taylor <eric.taylor1012@gmail.com>
2022-04-17 19:43:08 -0700

    added icon tags

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Weather-Dashboard
+
+This project is a weather dashboard that gives you information on today's weather including temperature, wind speed, humidity level, and uv index plus the 5 day forecast with the same features. 
+
+## Built With
+This page I used:
+• HTML
+• CSS
+• Javascript
+• Bootstrap
+
+## Concept 
+
+This project is designed to help users quickly identify the current and forecasted weather for cities around the world. 
+
+![LoadingPage](./maybe%20needed/Screen%20Shot%202022-04-17%20at%207.18.30%20PM.png)
+
+![LoadingPage](./maybe%20needed/Screen%20Shot%202022-04-17%20at%207.18.20%20PM.png)
+
+When a city is searched, today's weather including temperature, wind speed, humidity level, and uv index plus the 5 day forecast with the same features displayed. 
+
+After a city is searched, it gets displayed underneath the search bar. 
+
+## Successes
+
+I felt successful in pulling API's and successfully displaying multiple features on the same page.
+
+## Challenges
+
+I struggled with adding the icons, even though I successfully pulled the correct icon tag. I also, struggled with getting past displays to display on the page. I have the correct information in my console.log but not sure where to get the new information to display on the page. 
--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var humidityEl = document.querySelector('#humidity');
 var uvEl = document.querySelector('#uv-index');
 var ApiKey = "fa7d52ba051ca9699cdf472c349d267c";
 var newCityBtn = document.getElementsByClassName("list")
+var mainIcon = document.getElementById("mainIcon")
+
 
 
 //searches
@@ -14,37 +16,53 @@ var pastSearches = []
 
 //forecast data
 var forecastDatesEl = document.getElementById("forecast-dates")
+var iconEl = document.getElementById("icon")
 var forecastTempEl = document.getElementById("forecast-temperature")
 var forecastWindEl = document.getElementById("forecast-wind-speed")
 var forecastHumidityEl = document.getElementById("forecast-humidity")
 
 //forecast day 2 
 var forecast1DatesEl = document.getElementById("forecast-dates1")
+var icon1El = document.getElementById("icon1")
 var forecast1TempEl = document.getElementById("forecast-temperature1")
 var forecast1WindEl = document.getElementById("forecast-wind-speed1")
 var forecast1HumidityEl = document.getElementById("forecast-humidity1")
 
 //forecast day 3 
 var forecast2DatesEl = document.getElementById("forecast-dates2")
+var icon2El = document.getElementById("icon2")
 var forecast2TempEl = document.getElementById("forecast-temperature2")
 var forecast2WindEl = document.getElementById("forecast-wind-speed2")
 var forecast2HumidityEl = document.getElementById("forecast-humidity2")
 
 //forecast day 4 
 var forecast3DatesEl = document.getElementById("forecast-dates3")
+var icon3El = document.getElementById("icon3")
 var forecast3TempEl = document.getElementById("forecast-temperature3")
 var forecast3WindEl = document.getElementById("forecast-wind-speed3")
 var forecast3HumidityEl = document.getElementById("forecast-humidity3")
 
 //forecast day 5 
 var forecast4DatesEl = document.getElementById("forecast-dates4")
+var icon4El = document.getElementById("icon4")
 var forecast4TempEl = document.getElementById("forecast-temperature4")
 var forecast4WindEl = document.getElementById("forecast-wind-speed4")
 var forecast4HumidityEl = document.getElementById("forecast-humidity4")
 
 
   // CITY DATA
-  function apiParameters() {
+  function apiParameters(event) {
+    console.log(event.target)
+
+    //Check the event.target.innerHTML
+    event.target.innerHTML
+    //if that value is equal to Search, we know the user clicked the search button, so we should use city.value in our queryURL
+    if (event.target === pastSearches) {
+
+    }
+    //if that value is something else, we should use event.target.HTML since it will be the city value already clicked
+
+
     let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city.value + "&appid=" + ApiKey; 
       fetch(queryURL)
       .then(function(getApi){
@@ -56,8 +74,8 @@ var forecast4HumidityEl = document.getElementById("forecast-humidity4")
         var unixTime = data.dt * 1000
         var today = moment(unixTime).format("M/D/YYYY")
 
-      localCityEl.textContent = data.name + " " + today;
-      windSpeedEl.textContent = "Wind: " + data.wind.speed + " MPH";
+      localCityEl.textContent = data.name + " " + today + " " + data.weather[0].icon;
+      windSpeedEl.textContent = "Wind Speed: " + data.wind.speed + " MPH";
       humidityEl.textContent = "Humidity: " + data.main.humidity + " %";
       
       // localStorage.setItem("city", JSON.stringify(data.name))
@@ -96,7 +114,7 @@ var forecast4HumidityEl = document.getElementById("forecast-humidity4")
       var lon1 = data1.lon;
       
       // 5 Day Forecast
-      return fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat1 + "&lon=" + lon1 + "&appid=" + ApiKey)
+      return fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat1 + "&lon=" + lon1 + "&units=imperial&appid=" + ApiKey)
       }) .then(function (forecast) {
           return forecast.json();
       })  
@@ -126,29 +144,29 @@ var forecast4HumidityEl = document.getElementById("forecast-humidity4")
 
       for (var i = 8; i < forecastData.list[i]; i + 8);
 
-      forecastDatesEl.textContent = tomorrow;
-      forecastTempEl.textContent = "Temp: " + forecastData.list[8].main.temp + " deg C"
-      forecastWindEl.textContent = "Wind: " + forecastData.list[8].wind.speed + " MPH"
+      forecastDatesEl.textContent = tomorrow + " " + forecastData.list[8].weather[0].icon;
+      forecastTempEl.textContent = "Temp: " + forecastData.list[8].main.temp + " deg F"
+      forecastWindEl.textContent = "Wind Speed: " + forecastData.list[8].wind.speed + " MPH"
       forecastHumidityEl.textContent = "Humidity: " + forecastData.list[8].main.humidity + " %"
 
-      forecast1DatesEl.textContent = secondDay;
-      forecast1TempEl.textContent = "Temp: " + forecastData.list[16].main.temp + " deg C"
-      forecast1WindEl.textContent = "Wind: " + forecastData.list[16].wind.speed + "MPH"
+      forecast1DatesEl.textContent = secondDay + " " + forecastData.list[16].weather[0].icon;
+      forecast1TempEl.textContent = "Temp: " + forecastData.list[16].main.temp + " deg F"
+      forecast1WindEl.textContent = "Wind Speed: " + forecastData.list[16].wind.speed + "MPH"
       forecast1HumidityEl.textContent = "Humidity: " + forecastData.list[16].main.humidity + " %"
       
-      forecast2DatesEl.textContent = thirdDay;
-      forecast2TempEl.textContent = "Temp: " + forecastData.list[24].main.temp + " deg C"
-      forecast2WindEl.textContent = "Wind: " + forecastData.list[24].wind.speed + "MPH"
+      forecast2DatesEl.textContent = thirdDay + " " + forecastData.list[24].weather[0].icon;
+      forecast2TempEl.textContent = "Temp: " + forecastData.list[24].main.temp + " deg F"
+      forecast2WindEl.textContent = "Wind Speed: " + forecastData.list[24].wind.speed + "MPH"
       forecast2HumidityEl.textContent = "Humidity: " + forecastData.list[24].main.humidity + " %"
 
-      forecast3DatesEl.textContent = fourthDay;
-      forecast3TempEl.textContent = "Temp: " + forecastData.list[32].main.temp + " deg C"
-      forecast3WindEl.textContent = "Wind: " + forecastData.list[32].wind.speed + "MPH"
+      forecast3DatesEl.textContent = fourthDay + " " + forecastData.list[32].weather[0].icon;
+      forecast3TempEl.textContent = "Temp: " + forecastData.list[32].main.temp + " deg F"
+      forecast3WindEl.textContent = "Wind Speed: " + forecastData.list[32].wind.speed + "MPH"
       forecast3HumidityEl.textContent = "Humidity: " + forecastData.list[32].main.humidity + " %"
 
-      forecast4DatesEl.textContent = fifthDay;
-      forecast4TempEl.textContent = "Temp: " + forecastData.list[39].main.temp + " deg C"
-      forecast4WindEl.textContent = "Wind: " + forecastData.list[39].wind.speed + "MPH"
+      forecast4DatesEl.textContent = fifthDay + " " + forecastData.list[39].weather[0].icon;
+      forecast4TempEl.textContent = "Temp: " + forecastData.list[39].main.temp + " deg F"
+      forecast4WindEl.textContent = "Wind Speed: " + forecastData.list[39].wind.speed + "MPH"
       forecast4HumidityEl.textContent = "Humidity: " + forecastData.list[39].main.humidity + " %"
      
   
@@ -171,13 +189,10 @@ var forecast4HumidityEl = document.getElementById("forecast-humidity4")
   liList.append(pastSearches[0])
   li.classList.add("list")
   ul.append(li)
+
+  li.addEventListener("click", apiParameters)
   }
 
-
-  function myCityBtn () {
-    var newCityBtn = document.getElementsByClassName("list")
-  }
 
   searchBtn.addEventListener('click', apiParameters);
   searchBtn.addEventListener('click', pushCities);
-  newCityBtn.addEventListener('click', apiParameters)
